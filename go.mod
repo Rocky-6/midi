45610690868799c628ad6406f7b214b3a6d1e584
Rocky-6 <imu387058@gmail.com>
2022-07-27 13:01:27 +0900

    Create midi file

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/Rocky-6/midi
 
 go 1.18
 
-require gitlab.com/gomidi/midi v1.23.7
+require gitlab.com/gomidi/midi/v2 v2.0.23
--- main.go
+++ main.go
@@ -2,43 +2,40 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
 
-	"gitlab.com/gomidi/midi/writer"
+	"gitlab.com/gomidi/midi/v2"
+	"gitlab.com/gomidi/midi/v2/gm"
+	"gitlab.com/gomidi/midi/v2/smf"
 )
 
 func main() {
-	dir, _ := os.Getwd()
-	f := filepath.Join(dir, "smf-test.mid")
-
-	if _, err := os.Stat(f); os.IsExist(err) {
-		os.Remove(f)
-	}
-
-	err := writer.WriteSMF(f, 1, func(wr *writer.SMF) error {
-		writer.TrackSequenceName(wr, "doremi")
-		writer.TempoBPM(wr, 120)
-		writer.ProgramChange(wr, 1)
-
-		writer.NoteOn(wr, 60, 100)
-		wr.SetDelta(960)
-		writer.NoteOff(wr, 60)
-
-		writer.NoteOn(wr, 62, 100)
-		wr.SetDelta(960)
-		writer.NoteOff(wr, 62)
-
-		writer.NoteOn(wr, 64, 100)
-		wr.SetDelta(960)
-		writer.NoteOff(wr, 64)
-		writer.EndOfTrack(wr)
-		return nil
-	})
-
+	s := mkSMF()
+	err := s.WriteFile("test.mid")
 	if err != nil {
-		fmt.Printf("could not write SMF file %v\n", f)
+		fmt.Printf("error")
 		return
 	}
 
+}
+
+func mkSMF() *smf.SMF {
+	var (
+		clock = smf.MetricTicks(96)
+		tr    smf.Track
+	)
+
+	tr.Add(0, smf.MetaMeter(3, 4))
+	tr.Add(0, smf.MetaTempo(140))
+	tr.Add(0, smf.MetaInstrument("Trumpet"))
+	tr.Add(0, midi.ProgramChange(0, gm.Instr_Trumpet.Value()))
+	tr.Add(0, midi.NoteOn(0, midi.Ab(3), 120))
+	tr.Add(clock.Ticks8th(), midi.NoteOn(0, midi.C(4), 120))
+	tr.Add(clock.Ticks4th()*2, midi.NoteOff(0, midi.Ab(3)))
+	tr.Add(0, midi.NoteOff(0, midi.C(4)))
+	tr.Close(0)
+
+	s := smf.New()
+	s.TimeFormat = clock
+	s.Add(tr)
+	return s
 }
